2ca9e75617697604fa2b3f306ccae3234b9895f5
Flenix <hedgesj@hotmail.co.uk>
2015-03-26 20:08:46 +0000

    Coding entirely in GitHub isn't fun.

--- src/main/java/co/uk/silvania/cOre/blocks/MetaOre.java
+++ src/main/java/co/uk/silvania/cOre/blocks/MetaOre.java
@@ -9,6 +9,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import co.uk.silvania.cOre.COre;
+import java.util.Random;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
